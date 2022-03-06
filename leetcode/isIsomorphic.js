8b82aee86e4f0a8dac9a08d8c54787b95bdcfbfc
lilyruth <doodlepath@gmail.com>
2022-03-06 17:20:30 -0600

    refactored isIsomorphic tests

--- leetcode/isIsomorphic.js
+++ leetcode/isIsomorphic.js
@@ -13,10 +13,9 @@ var isIsomorphic = function(s, t) {
      if (hash2[charT]) hash2[charT].push(i);
      else hash2[charT] = [i];
     }
-    console.log(hash1, hash2)
+
     let sVals = Object.values(hash1).sort((a,b) => a -b);
     let tVals = Object.values(hash2).sort((a,b) => a -b);
-    console.log('hash1', sVals, 'hash2', tVals)
     
     let same = true;
 
@@ -32,32 +31,23 @@ var isIsomorphic = function(s, t) {
 };
 
 const testIsIsomorphic = () => {
-
-  let test1 = isIsomorphic('egg', 'add');
-  if (test1) console.log('Passed, egg and add are isomorphic');
-  else console.log('Failed, expected to be true');
-
-  let test2 = isIsomorphic('foo', 'bar');
-  if (test2) console.log('Failed, foo and bar are not isomorphic');
-  else console.log('Passed, foo and bar are not isomorphic');
-
-  let test3 = isIsomorphic('paper', 'title');
-  if (test3) console.log('Passed, paper and title are isomorphic');
-  else console.log('Failed, expected to be true');
-
-  let test4 = isIsomorphic('bbbaaaba', 'aaabbbba');
-  if (test4) console.log('Failed, bbbaaaba and aaabbbba are not isomorphic');
-  else console.log('Passed, bbbaaaba and aaabbbba are not isomorphic');
-
-  let test5 = isIsomorphic('badc', 'baba');
-  if (test5) console.log('Failed, badc and baba are not isomorphic');
-  else console.log('Passed, badc and baba are not isomorphic');
-
-  let test6 = isIsomorphic('13', '42');
-  if (test6) console.log('Passed, 13 and 42 are isomorphic');
-  else console.log('Failed, expected to be true');
+  let testCases = [
+   {tests: ['egg', 'add'], expected: true}, 
+   {tests: ['foo', 'bar'], expected: false},
+   {tests: ['paper', 'title'], expected: true},
+   {tests: ['bbbaaaba', 'aaabbbba'], expected: false},
+   {tests: ['badc', 'baba'], expected: false},
+   {tests: ['13', '42'], expected: true}
+  ]
+  
+  testCases.forEach(item => {
+   let args = item.tests;
+   console.log(
+    isIsomorphic(...args) === item.expected, 
+    `${args} expected to be ${item.expected}`
+   )
+  })
 
 }
-"badc"
-"baba"
+
 console.log(testIsIsomorphic());
