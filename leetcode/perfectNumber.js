be0d8c2eb42aebc016278cc29886d17665778ecc
lilyruth <doodlepath@gmail.com>
2022-03-26 13:05:30 -0500

    revised perfect number

--- leetcode/perfectNumber.js
+++ leetcode/perfectNumber.js
@@ -2,15 +2,15 @@ var checkPerfectNumber = function(num) {
     
  if (num === 1) return false;
  
- let divisors = [];
+ let divisors = 0;
  
  for (let i = 1; i <= Math.sqrt(num); i++) {
      let divisor = parseInt(num/i);
      if (divisor * i === num) {
-         divisors.push(i);
-         if (divisor < num && divisor !== i) divisors.push(divisor);
+         divisors += i;
+         if (divisor < num && divisor !== i) divisors += divisor;
      }
  }
- return divisors.reduce((a,b) => a + b) === num;
+ return divisors === num;
 
 };
