bba05eb07eefb4a5a86594f7b534a1489cbc99f1
lilyruth <doodlepath@gmail.com>
2022-03-30 18:33:52 -0500

    made one small change to Two Sum II

--- leetcode/twoSumII.js
+++ leetcode/twoSumII.js
@@ -5,7 +5,7 @@ var twoSum = function(numbers, target) {
  let pointer1 = 0;
  let pointer2 = numbers.length - 1;
  
- while (pointer1 <= pointer2) {
+ while (pointer1 < pointer2) {
      
      if (numbers[pointer1] + numbers[pointer2] === target) {
          return [pointer1 + 1, pointer2 + 1];
